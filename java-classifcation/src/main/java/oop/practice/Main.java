3f271e440ea227b6eb87fc8e0cceec5a438f0092
Nicolae M. <nicolaemargamd@gmail.com>
2024-10-13 23:13:44 +0300

    Added output in a json file

--- java-classifcation/src/main/java/oop/practice/Main.java
+++ java-classifcation/src/main/java/oop/practice/Main.java
@@ -1,6 +1,8 @@
 package oop.practice;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -8,22 +10,32 @@ import com.google.gson.GsonBuilder;
 public class Main {
     public static void main(String[] args) {
         FileReader filer = new FileReader();
+        // Read the input.json file
         filer.readJsonFile("java-classifcation/src/main/resources/input.json");
         filer.splitJsonIntoObjects();
         // Print out the string file
         String dataString = filer.getIndividualJsons().getFirst();
-
         // Convert String Data to object
         GsonBuilder builder = new GsonBuilder(); 
         Gson gson = builder.create(); 
         DataWrapper dataC = gson.fromJson(dataString, DataWrapper.class);
+
+        // Setup a query to request data by classification
         View view = new View(dataC);
         view.runQuery(dataC);
+        // Get the output and convert it to json format
         DataWrapper output = new DataWrapper();
         output.setData(view.getOutput());
         String jsonOutput = gson.toJson(output);
-        System.out.println(jsonOutput);
 
-      
+        // Write into the output.json file
+        System.out.println("The JSON format output:");
+        System.out.println(jsonOutput);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("java-classifcation/src/main/resources/output.json"))) {
+            writer.write(jsonOutput);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Program ended");
     }
 }
