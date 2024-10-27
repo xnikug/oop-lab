4502cd375c7495b8c074a23724bf07bc8a077f8b
Nicolae M. <nicolaemargamd@gmail.com>
2024-10-28 01:47:08 +0200

    Added multiple main args input handling (bonus task)

--- java-classifcation/src/main/java/oop/intermediate/TextParsingMain.java
+++ java-classifcation/src/main/java/oop/intermediate/TextParsingMain.java
@@ -5,8 +5,15 @@ import java.io.IOException;
 import oop.intermediate.text_parsing.*;
 public class TextParsingMain {
     public static void main(String[] args) throws IOException{
-        System.out.println(args[0]);
-        TextData inputData = new TextData(args[0]);
-        System.out.println(inputData.toString());
+        // Multiple files processing
+        if (args.length == 0){
+            System.out.println("No arguments have been passed");
+            return;
+        }
+        System.out.println("The TextData objects:");
+        for (int i = 0; i < args.length; i++){
+            TextData inputData = new TextData(args[i]);
+            System.out.println(inputData.toString());
+        }
     }
 }
