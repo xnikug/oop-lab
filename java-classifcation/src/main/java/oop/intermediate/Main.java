ac8d436d94b0d280bd21c9832835a6d3216461a9
Nicolae M. <nicolaemargamd@gmail.com>
2024-10-28 00:05:47 +0200

    Added the Text Parsing package

--- java-classifcation/src/main/java/oop/intermediate/Main.java
+++ java-classifcation/src/main/java/oop/intermediate/Main.java
@@ -1,9 +1,10 @@
 package oop.intermediate;
-import oop.intermediate.display.Display;
 
+import oop.intermediate.display.Display;
+import oop.intermediate.text_parsing.*;
 public class Main {
-    public static void main(String[] args) {
-        // Instantiate 3 display objects
+    public static void runTask1(){
+        // Instantiate 3 display objects with random attributes
         Display display1 = new Display(1920, 1080, 401, "Display1");
         Display display2 = new Display(2560, 1440, 500, "Display2");
         Display display3 = new Display(3840, 2160, 218, "Display3");
@@ -14,5 +15,10 @@ public class Main {
         display1.compareWithMonitor(display3);
         System.out.println();
         display2.compareWithMonitor(display3);
+    }
+    public static void main(String[] args) {
+        runTask1();
+        TextData textInput = new TextData("./src/main/resources/input.txt");
+        System.out.println(textInput.toString());
     }
 }
