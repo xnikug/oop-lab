33b4ae129cc175a6007ab75f3147a0a0fefb7e87
Nicolae M. <nicolaemargamd@gmail.com>
2024-11-04 22:26:04 +0200

    Suppressed unused instances warning

--- java-advanced-tasks/demo/src/main/java/oop/advanced/task3/Main3.java
+++ java-advanced-tasks/demo/src/main/java/oop/advanced/task3/Main3.java
@@ -10,18 +10,22 @@ import oop.advanced.coffee_specification.SyrupType;
 public class Main3 {
     public static void main(String[] args) {
         System.out.println("Making a Cappuccino:");
+        @SuppressWarnings("unused")
         Cappuccino cappuccino = Cappuccino.makeCappuccino(Intensity.NORMAL, 50);
         System.out.println();
 
         System.out.println("Making a Pumpkin Spice Latte:");
+        @SuppressWarnings("unused")
         PumpkinSpiceLatte pumpkinSpiceLatte = PumpkinSpiceLatte.makePumpkinSpiceLatte(Intensity.STRONG, 60, 20);
         System.out.println();
 
         System.out.println("Making an Americano:");
+        @SuppressWarnings("unused")
         Americano americano = Americano.makeAmericano(Intensity.LIGHT, 250);
         System.out.println();
 
         System.out.println("Making a Syrup Cappuccino:");
+        @SuppressWarnings("unused")
         SyrupCappuccino syrupCappuccino = SyrupCappuccino.makeSyrupCappuccino(Intensity.NORMAL, 50, SyrupType.CARAMEL);
     }
 }
